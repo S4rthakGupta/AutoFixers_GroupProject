956e14007ddd94612de599dd0c0ac9750d832b13
Abhishek Ajay Chachad <abhishek.chachad@gmail.com>
2024-08-16 01:16:35 -0400

    comments

--- Main Code/GetCustomers.php
+++ Main Code/GetCustomers.php
@@ -1,15 +1,15 @@
 <?php
 
-
-
-
 // Include database connection
 include 'ConnectDB.php';
 
+// Get all records from Customers table and execute the query on second line below
 $sql = "SELECT * FROM Customers";
 $result = $conn->query($sql);
 
+// Check if there are any records returned from the query
 if ($result->num_rows > 0) {
+    // Using while loop for setting the data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr>
                 <td>" . htmlspecialchars($row["CustomerName"]) . "</td>
@@ -23,6 +23,7 @@ if ($result->num_rows > 0) {
               </tr>";
     }
 } else {
+    // Display No customers found if no records returned from database
     echo "<tr><td colspan='4'>No customers found</td></tr>";
 }
 
